db891c4d2de07c4f80385c39ec06bdc405dd3b63
MartoIT <88441320+MartoIT@users.noreply.github.com>
2023-03-06 09:54:38 +0200

    add search bar

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,20 @@
 import Footer from "./componets/Footer";
 import Header from "./componets/Header";
+import Search from "./componets/Search";
 
 function App() {
   return (
     <>
       <Header />
+
+      <main className="main">
+
+      <section className="card users-container">
+        <Search />
+
+      </section>
+  
+      </main>
 
       <Footer />
     </>
